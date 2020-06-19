d0a9d4e8c0d097cd3a1310627d150d29256afd7b
Erendis Near <mailbox.novaka@gmail.com>
2020-06-19 11:55:47 +0200

    Create 03_circle.cpp

---
+++ Lesson03/03_circle.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+const float pi = 3.141592;
+
+void CalculateAreaAndCircumference() {
+	unsigned int radius = 0;
+
+	cout << "Enter a positive integer as the radius of the circle: " << endl;
+	cin >> radius;
+
+	float area = 0;
+	area = radius * radius * pi;
+
+	float circumference = 0;
+	circumference = 2 * radius * pi;
+
+	cout << "The area of the circle is: " << area << endl
+		<< "The circumference of the circle is: " << circumference << endl;
+}
+
+int main() {	
+	CalculateAreaAndCircumference();
+
+	return 0;
+} 
