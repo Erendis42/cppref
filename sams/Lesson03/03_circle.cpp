720972416839c86aace8a895f66af446ee19d58f
Adrienn Novák <mailbox.novaka@gmail.com>
2020-06-23 11:14:32 +0200

    Update 03_circle.cpp

--- sams/Lesson03/03_circle.cpp
+++ sams/Lesson03/03_circle.cpp
@@ -3,13 +3,17 @@ using namespace std;
 
 const float PI = 3.141592;
 
+inline int Square(int inputNum) {
+	return inputNum * inputNum;
+}
+
 void CalculateAreaAndCircumference() {
 	unsigned int radius = 0;
 
 	cout << "Enter a positive integer as the radius of the circle: " << endl;
 	cin >> radius;
 
-	float area{ radius * radius * PI };
+	float area{ Square(radius) * PI };
 	float circumference{ 2 * radius * PI };
 
 	cout << "The area of the circle is: " << area << endl \
