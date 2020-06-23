9d8c564be0b67bd90e29b5bcebb6238a62c9876c
Adrienn Novák <mailbox.novaka@gmail.com>
2020-06-23 11:55:18 +0200

    Create 01_overload.cpp

---
+++ sams/Lesson07/01_overload.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+const double Pi = 3.1416;
+
+double Volume(double radius);
+double Volume(double radius, double height);
+
+int main()
+{
+	cout << "Enter radius: ";
+	double radius = 0;
+	cin >> radius;
+
+	cout << "Do you want to calculate the volume of a cylinder or a sphere? (c/s) ";
+	char userInput = 'c';
+	cin >> userInput;
+
+	if (userInput == 'c')
+	{
+		cout << "Enter the height of the cylinder: ";
+		double height = 0;
+		cin >> height;
+
+		cout << "The volume is: " << Volume(radius, height) << endl;
+	}
+	else
+		cout << "The volume is: " << Volume(radius) << endl;
+}
+
+double Volume(double radius)
+{
+	return (4 * Pi * radius * radius * radius) / 3;
+}
+
+double Volume(double radius, double height)
+{
+	return Pi * radius * radius * height;
+}
