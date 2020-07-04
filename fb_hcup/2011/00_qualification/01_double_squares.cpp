7154858f02cac20826a5d36fa8646f2a78465a39
Adrienn Novák <mailbox.novaka@gmail.com>
2020-07-04 12:28:49 +0200

    Update 01_double_squares.cpp

--- fb_hcup/2011/00_qualification/01_double_squares.cpp
+++ fb_hcup/2011/00_qualification/01_double_squares.cpp
@@ -3,61 +3,73 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <map>
 
 using namespace std;
 
-void main(int argc, char* argv[]) {
+void ReadFile(std::ifstream& infile, int& numberOfIntegers, std::vector<int>& vectorOfIntegers);
+void GeneratePrimes(int& low, int& high, std::vector<int>& primes);
 
-//    if (argc == 2) {
+int main(int argc, char* argv[]) {
+
+    //    if (argc == 2) {
 
         //string filename = argv[1];
-        string filename = ".\\double_squares_sample_input.txt";
-        ifstream infile(filename);
-        
-        int numberOfIntegers = 0;
-        vector<int> vectorOfIntegers;
+    string filename = ".\\double_squares_sample_input.txt";
+    //string filename = ".\\double_squares.txt";
+    ifstream infile(filename);
 
-        // read the whole thing
-        // number of ints goes to numberOfIntegers, the rest goes into vectorOfIntegers
-        ReadFile(infile, numberOfIntegers, vectorOfIntegers);
+    int numberOfIntegers = 0;
+    vector<int> vectorOfIntegers;
 
-        // process numbers one by one to count the possible ways to write them as the sum of
-        // two square numbers
+    ReadFile(infile, numberOfIntegers, vectorOfIntegers);
 
-        for (int i : vectorOfIntegers)
-        {
-            // probably better with hashmap or something
-            vector<int[2]> vectorOfDoublePrimeFactors;
-            
-            int indexOfFactors;
-            int dividend = i;
-            int divisor = 0;
+    vector<int> primes;
+    int low, high = 0;
 
-            while(dividend > 0)
-            {
-                // generate prime numbers until its not greater than the number
-                // (https://www.programiz.com/cpp-programming/examples/prime-number-intervals)
+    for (int i : vectorOfIntegers)
+    {
+        low = high;
+        high = i;
 
-                // check with loop for power of each prime factor
-
-                // get subsets of 2 numbers with powers greater than or equal to 2
-
-                // count the number of these subsets
-
-                // profit! :)
-            }
+        if (high > low) {
+            GeneratePrimes(low, high, primes);
         }
 
-        infile.close();
- /*   }
-      else      
-      {
-        cout << "Wrong number of parameters!" << endl;
-      }
- */
+        map<int, int> vectorOfPrimeFactors;
+        int dividend = i;
+        int divisor = 0;
+
+        while (dividend > 0)
+        {
+            // check with loop for power of each prime factor
+
+            // get subsets of 2 numbers with powers greater than or equal to 0
+
+            // count the number of these subsets
+
+            // profit! :)
+        }
+    }
+
+    for (int i : primes) {
+        cout << i << " ";
+    }
+
+    cout << endl;
+
+    infile.close();
+    /*   }
+         else
+         {
+           cout << "Wrong number of parameters!" << endl;
+         }
+    */
+
+    return 0;
 }
 
-void ReadFile(std::ifstream& infile, int& numberOfIntegers, std::vector<int>& integers)
+void ReadFile(std::ifstream& infile, int& numberOfIntegers, std::vector<int>& vectorOfIntegers)
 {
     int number = 0;
     if (infile.is_open()) {
@@ -66,15 +78,43 @@ void ReadFile(std::ifstream& infile, int& numberOfIntegers, std::vector<int>& in
         {
             if (first) {
                 numberOfIntegers = number;
+                first = false;
             }
             else
             {
-                integers.push_back(number);
+                vectorOfIntegers.push_back(number);
             }
         }
     }
     else
     {
         cout << "file can't be opened" << endl;
+    }
+}
+
+// (https://www.programiz.com/cpp-programming/examples/prime-number-intervals)
+void GeneratePrimes(int& low, int& high, std::vector<int>& primes)
+{
+    bool isPrime;
+
+    while (low < high)
+    {
+        isPrime = true;
+
+        for (int i = 2; i <= low / 2; ++i)
+        {
+            if (low % i == 0)
+            {
+                isPrime = false;
+                break;
+            }
+        }
+
+        if (isPrime)
+        {
+            primes.push_back(low);
+        }
+
+        ++low;
     }
 }
