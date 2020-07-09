53b52d15b593bef447d2ac9da5731de7cc7607ab
Erendis Near <mailbox.novaka@gmail.com>
2020-07-09 18:35:56 +0200

    Update 01_double_squares.cpp

--- fb_hcup/2011/00_qualification/01_double_squares.cpp
+++ fb_hcup/2011/00_qualification/01_double_squares.cpp
@@ -1,120 +1,66 @@
 #include <iostream>
-#include <fstream>
-#include <sstream>
-#include <string>
 #include <vector>
-#include <map>
 
 using namespace std;
 
-void ReadFile(std::ifstream& infile, int& numberOfIntegers, std::vector<int>& vectorOfIntegers);
-void GeneratePrimes(int& low, int& high, std::vector<int>& primes);
+int CountDoubleSquares(int n);
+vector<vector<int>> GenerateSubsets(int n);
 
-int main(int argc, char* argv[]) {
+vector<int> primes;
 
-    //    if (argc == 2) {
+int main()
+{
+    int input[] = {10, 25, 3, 0, 1};
 
-        //string filename = argv[1];
-    string filename = ".\\double_squares_sample_input.txt";
-    //string filename = ".\\double_squares.txt";
-    ifstream infile(filename);
-
-    int numberOfIntegers = 0;
-    vector<int> vectorOfIntegers;
-
-    ReadFile(infile, numberOfIntegers, vectorOfIntegers);
-
-    vector<int> primes;
-    int low, high = 0;
-
-    for (int i : vectorOfIntegers)
+    for (int i = 0; i < 5; i++)
     {
-        low = high;
-        high = i;
-
-        if (high > low) {
-            GeneratePrimes(low, high, primes);
-        }
-
-        map<int, int> vectorOfPrimeFactors;
-        int dividend = i;
-        int divisor = 0;
-
-        while (dividend > 0)
+        if (input[i] == 0)
         {
-            // check with loop for power of each prime factor
-
-            // get subsets of 2 numbers with powers greater than or equal to 0
-
-            // count the number of these subsets
-
-            // profit! :)
+            cout << "0" << endl;
+        }
+        else
+        {
+            cout << CountDoubleSquares(input[i]) << endl;
         }
     }
-
-    for (int i : primes) {
-        cout << i << " ";
-    }
-
-    cout << endl;
-
-    infile.close();
-    /*   }
-         else
-         {
-           cout << "Wrong number of parameters!" << endl;
-         }
-    */
 
     return 0;
 }
 
-void ReadFile(std::ifstream& infile, int& numberOfIntegers, std::vector<int>& vectorOfIntegers)
+int CountDoubleSquares(int n)
 {
-    int number = 0;
-    if (infile.is_open()) {
-        bool first = true;
-        while (infile >> number)
+    vector<vector<int>> subsets = GenerateSubsets(n);
+    int counter = 0;
+
+    for (vector<int> pair: subsets)
+    {
+        int a = pair[0];
+        int b = pair[1];
+
+        if (pow(a, 2) + pow(b, 2) == n)
         {
-            if (first) {
-                numberOfIntegers = number;
-                first = false;
-            }
-            else
-            {
-                vectorOfIntegers.push_back(number);
-            }
+            //cout << a << "^2 + " << b << "^2 = " << n << endl;
+            counter++;
         }
     }
-    else
-    {
-        cout << "Unable to open file." << endl;
-    }
+
+    return counter;
 }
 
-// (https://www.programiz.com/cpp-programming/examples/prime-number-intervals)
-void GeneratePrimes(int& low, int& high, std::vector<int>& primes)
-{
-    bool isPrime;
+vector<vector<int>> GenerateSubsets(int n) {
+    vector<vector<int>> subsets;
+    vector<int> pair;
 
-    while (low < high)
+    for (int i = 0; i < n; i++)
     {
-        isPrime = true;
-
-        for (int i = 2; i <= low / 2; ++i)
+        for (int j = i+1; j < n+1; j++)
         {
-            if (low % i == 0)
-            {
-                isPrime = false;
-                break;
-            }
+            pair.clear();
+            pair.push_back(i);
+            pair.push_back(j);
+            subsets.push_back(pair);
         }
-
-        if (isPrime)
-        {
-            primes.push_back(low);
-        }
-
-        ++low;
     }
+
+    return subsets;
 }
